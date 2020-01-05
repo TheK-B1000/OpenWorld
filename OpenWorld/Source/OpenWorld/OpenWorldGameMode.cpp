2bee44c1234cfeec3f5c83c973f61eedd405ca8c
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2020-01-05 08:40:06 -0500

    01 - Initial Commit

---
+++ OpenWorld/Source/OpenWorld/OpenWorldGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "OpenWorldGameMode.h"
+#include "OpenWorldCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AOpenWorldGameMode::AOpenWorldGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ OpenWorld/Source/OpenWorld/OpenWorldGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenWorldGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOpenWorldGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOpenWorldGameMode();
+};
+
+
+
